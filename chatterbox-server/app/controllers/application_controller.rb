68111812b2e4e06531830a71ffbad8bbe03b8667
Leandro Dos Santos Afonso <leandro.afonso@hotmail.com>
2022-08-13 21:15:58 -0400

    Completed Assignment

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    Message.all.to_json
+  end
+
+  post '/messages' do
+    msg = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    msg.to_json
+  end
+
+  patch "/messages/:id" do
+    msg = Message.find(params[:id])
+    msg.update(body: params[:body] )
+    msg.to_json
+  end
+
+  delete '/messages/:id' do
+    msg = Message.find(params[:id])
+    msg.destroy.to_json
+  end
   
 end
